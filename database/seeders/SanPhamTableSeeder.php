1b84c74288ad63bad54cfb6e348de80714a422b3
VyDuong162 <69305591+VyDuong162@users.noreply.github.com>
2021-09-28 22:36:13 +0700

    migrate db

---
+++ database/seeders/SanPhamTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SanPhamTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
